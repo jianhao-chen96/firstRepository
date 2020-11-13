46c30ceb84b6c4b26ade6e207f684438c27f46ee
陈建豪 <910084520@qq.com>
2020-11-13 09:10:29 +0800

    上传工程文件到本地版本库

---
+++ OpenGLExercise01/OpenGLExercise01/Camera.h
@@ -0,0 +1,37 @@
+#pragma once
+
+//#ifndef CAMERA_H;
+//#define CAMERA_H;
+
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+
+
+class Camera
+{
+public:
+	Camera(glm::vec3 position, glm::vec3 target, glm::vec3 worldup);
+	Camera(glm::vec3 position, float pitch, float yaw, glm::vec3 worldup);
+
+	glm::vec3 Position;
+	glm::vec3 Forward;
+	glm::vec3 Right;
+	glm::vec3 Up;
+	glm::vec3 WorldUp;
+	float Pitch;
+	float Yaw;
+	float senseX = 0.001f;
+	float senseY = 0.001f;
+	float speedX = 0;
+	float speedY = 0;
+	float speedZ = 0;
+
+	glm::mat4 GetViewMatirx();
+	void ProcessMouseMovement(float deltaX, float deltaY);
+	void UpdateCameraPos();
+
+private:
+	void UpdateCameraVectors();
+};
+
+//#endif // !CAMERA_H;
